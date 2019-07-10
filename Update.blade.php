ceb45f00eb2e1c7412c56f61ef9663a17ded48a1
Samyak Jain <44099124+Samyak2607@users.noreply.github.com>
2019-07-10 10:24:24 +0530

    Add files via upload

---
+++ Update.blade.php
@@ -0,0 +1,115 @@
+@extends('layouts.header')
+
+@section('content')
+
+<!-- <div class="flash-message col-md-12">
+  @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+     @if (Session::has($msg))
+     <div class="alert alert-{{ $msg }} alert-dismissible">
+          <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+          {{ Session::get($msg) }}
+    </div>
+     
+      @endif
+  @endforeach
+</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Update Form') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('admin/update') }}">
+                        @csrf
+                       
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('username') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{Auth::user()->username }}" required autocomplete="username" autofocus>
+
+                                @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ Auth::user()->email }}" required autocomplete="email" autofocus>
+
+                                
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="contact" class="col-md-4 col-form-label text-md-right">{{ __('Contact Number') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="contact" type="tel" class="form-control @error('contact') is-invalid @enderror" name="contact" value="{{ Auth::user()->contact }}" required autocomplete="contact" autofocus>
+
+                               
+                            </div>
+                        </div>
+
+
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Update') }}
+                                </button>
+
+                                
+                                    <a class="btn btn-primary" href="{{ route('admin-dashboard') }}">
+                                        {{ __('Cancel') }}
+                                    </a>
+                                
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+
+<div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Update
+            <small><b>Your Information</b></small>
+          </h1>
+          
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+          <!-- Default box -->
+          <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title"><b>{{Auth::user()->designation}}</b></h3>
+              <div class="box-tools pull-right">
+                <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
+                <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              Update Your Info Soon.......
+            </div><!-- /.box-body -->
+            <!-- /.box-footer-->
+          </div><!-- /.box -->
+
+        </section><!-- /.content -->
+      </div><!-- /.content-wrapper -->
+
+
+@endsection
